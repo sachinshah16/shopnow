6be7a31d2961c2d7ed371a07ffcd48a86dbf8ca2
sachinshah16 <sachinshahwal@gmail.com>
2026-04-22 11:58:01 +0530

    added media files
    added ping url

--- shopnow/urls.py
+++ shopnow/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from products.views import SplashView
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +12,7 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('vendor/', include('vendors.urls')),
     path('orders/', include('orders.urls')),
+    path('keep-awaik/', lambda r:HttpResponse("working"))
 ]
 
 if settings.DEBUG:
